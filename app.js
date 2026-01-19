d5e19be2cc704679194236b28eb53ef9dd914640
Garrett Chitwood <gman@acvwa.com>
2026-01-18 18:20:52 -0800

    Corrected the error by changing !='s to ==.

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ console.log(" ");
 console.log("EXERCISE 2:\n==========\n");
 
 for(let i = 0; i <= 100; i++) {
-    if (i % 3 != 0 && i % 5 != 0) {
+    if (i % 3 == 0 && i % 5 == 0) {
         console.log("FIZZBUZZ");
-    } else if(i % 3 != 0) {
+    } else if(i % 3 == 0) {
         console.log("FIZZ");
-    } else if(i % 5 != 0) {
+    } else if(i % 5 == 0) {
         console.log("BUZZ");
     } else {
         console.log(i)
