f7ea96d7233f04fc763885e02c8f9ae4606e75c2
Garrett Chitwood <gman@acvwa.com>
2026-01-19 16:45:37 -0800

    Finished exercise 3's first two loops. Now working on the 2nd set of loops.

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ for(let i = 0; i <= 100; i++) {
         console.log(i)
     }
 }
- console.log(" ");
+console.log(" ");
 
  //Exercise 3 Section
- console.log("Exercise 3:\n==========\n");
+console.log("Exercise 3:\n==========\n");
 console.log("Exc.1 While Loop");
  let i = 0;
  while (i < 100) {
@@ -38,6 +38,27 @@ console.log("Exc.1 While Loop");
     }
     i++
  }
+console.log(" ");
 
- console.log("Exc.1 Do While Loop");
+console.log("Exc.1 Do While Loop");
  let n = 0;
+do {
+    if(n % 2 != 0) {
+    console.log(n);
+    }
+    n++
+} while (n < 100);
+console.log(" ")
+
+console.log("Exc.2 While Loop");
+ let x = 0;
+ while (x <=100) {
+    if (x % 3 == 0 && x % 5 == 0) {
+        console.log(x);
+    }
+    x++
+ }
+ console.log(" ")
+
+console.log("Exc.2 Do While Loop");
+ let y = 0;
