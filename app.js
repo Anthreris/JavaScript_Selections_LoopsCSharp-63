3d6b3bf02d5727ce39d4b2d8dbf406de442dd25f
Garrett Chitwood <gman@acvwa.com>
2026-01-19 18:20:03 -0800

    Completed the 3 basic exercises. Turning this in the current state due to being frusterating.

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ console.log(" ");
 
  //Exercise 3 Section
 console.log("Exercise 3:\n==========\n");
-console.log("Exc.1 While Loop");
+console.log("Exc3:1 While Loop");
  let i = 0;
  while (i < 100) {
     if (i % 2 != 0) {
@@ -40,7 +40,7 @@ console.log("Exc.1 While Loop");
  }
 console.log(" ");
 
-console.log("Exc.1 Do While Loop");
+console.log("Exc3:1 Do While Loop");
  let n = 0;
 do {
     if(n % 2 != 0) {
@@ -50,7 +50,7 @@ do {
 } while (n < 100);
 console.log(" ")
 
-console.log("Exc.2 While Loop");
+console.log("Exc3:2 While Loop");
  let x = 0;
  while (x <=100) {
     if (x % 3 == 0 && x % 5 == 0) {
@@ -60,10 +60,21 @@ console.log("Exc.2 While Loop");
     } if (x % 5 == 0) {
         console.log("BUZZ")
     }
-    x++
     console.log(x);
+    x++
  }
  console.log(" ")
 
-console.log("Exc.2 Do While Loop");
+console.log("Exc3:2 Do While Loop");
  let y = 0;
+ do {
+    if (y % 3 == 0 && y % 5 == 0) {
+        console.log("FIZZBUZZ");
+    } if (y % 3 == 0) {
+        console.log("FIZZ");
+    } if (y % 5 == 0) {
+        console.log("BUZZ");
+    }
+    console.log(y);
+    y++
+ } while (y <= 100);
